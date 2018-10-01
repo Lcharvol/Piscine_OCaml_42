1356c7ebe8e76af0460a43741b7712fea246929b
Lcharvol <charvolin0@gmail.com>
2018-10-01 14:41:54 +0200

    D00 > fix ex04

--- D00/ex04/ft_print_comb.ml
+++ D00/ex04/ft_print_comb.ml
@@ -1,16 +1,23 @@
 let rec ft_print_comb () =
-  let rec start nb =
-    if nb < 1000
-    then
-      begin
-        print_int nb;
-        print_string ", ";
-        start (nb + 1)
-      end
-    else
-      print_string "\n"
+  let rec start a b c =
+    print_int a;
+    print_int b;
+    print_int c;
+    if a < 7
+      then
+        begin
+          print_string ", ";
+          if c < 9
+            then start a b (c + 1)
+          else if b < 8
+            then start a (b + 1) (b + 2)
+          else
+            start (a + 1) (a + 2) (a + 3)
+        end
+      else
+        print_string "\n"
   in
-  start 0
+  start 0 1 2
 
 let () =
   ft_print_comb ()
