26e9a9e01f8ac5f5855a3779a3519cd2b20a6aff
Lcharvol <charvolin0@gmail.com>
2018-10-03 14:45:56 +0200

    Start D03

---
+++ D03/ex00/ft_graphics.ml
@@ -0,0 +1,38 @@
+type 'a tree = Nil | Node of 'a * 'a tree * 'a tree
+
+let square_size = 50
+
+let draw_square x y size =
+  Graphics.moveto (x - (size / 2)) (y - (size / 2));
+  Graphics.lineto (x - (size / 2)) ((y - (size / 2)) + size);
+  Graphics.lineto ((x - (size / 2)) + size) ((y - (size / 2)) + size);
+  Graphics.lineto ((x - (size / 2)) + size) (y - (size / 2));
+  Graphics.lineto (x - (size / 2)) (y - (size / 2));
+  Graphics.moveto x y
+
+let draw_text x y size text =
+    draw_square x y size;
+    Graphics.moveto (x - (square_size / 4)) (y - 5);
+    Graphics.draw_string text;
+    Graphics.moveto x y
+
+let draw_tree_node tree =
+  let rec draw_tree_node_rec tree x y = match tree with
+  | Nil -> draw_text x y square_size "Nil"
+  | Node(a, b, c) ->  draw_text x y square_size a;
+                      Graphics.moveto (x + (square_size / 2)) y;
+                      Graphics.lineto (x + square_size * 2) (y - square_size);
+                      draw_tree_node_rec b (x + (square_size * 2) + (square_size / 2)) (y - (square_size));
+                      Graphics.moveto (x + (square_size / 2)) y;
+                      Graphics.lineto (x + square_size * 2) (y + square_size);
+                      draw_tree_node_rec b (x + (square_size * 2) + (square_size / 2)) (y + (square_size))
+  in
+  draw_tree_node_rec tree 250 250
+
+
+
+let _ =
+  Graphics.open_graph "";
+  Graphics.set_window_title "ft_graphics";
+  draw_tree_node (Node ("Test", Nil, Nil));
+  print_char (Graphics.read_key())
