7f803e8039131b9b126d6a7cc4d59f31dc4acd55
Lcharvol <charvolin0@gmail.com>
2018-10-03 18:58:52 +0200

    Fix D01

--- D01/ex08/ft_sum.ml
+++ D01/ex08/ft_sum.ml
@@ -1,11 +1,15 @@
 let ft_sum f il iu =
-  let rec ft_sum_aux ac f il =
-    if il > iu
-    then ac
-    else ft_sum_aux (ac +. (f il)) f (il + 1)
+  if il > iu then nan else
+  let rec ft_sum_aux ac il =
+    if il > iu then ac
+    else ft_sum_aux (ac +. (f il)) (il + 1)
   in
-  ft_sum_aux 0. f il
+  ft_sum_aux 0. il
 
 let () = 
     print_float (ft_sum (fun i -> float_of_int (i * i)) 1 10);
+    print_char '\n';
+    print_float (ft_sum (fun i -> float_of_int (i * i)) 4 100);
+    print_char '\n';
+    print_float (ft_sum (fun i -> float_of_int (i * i)) 4 1);
     print_char '\n'
